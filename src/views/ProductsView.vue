d429c6010aa82c22a697d5d357c385ec261f6060
MrAtaru <zrx655@gmail.com>
2023-08-08 13:38:30 +0700

    a

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Halaman Product</h1>
+    <div v-for="data in products" :key="data.id">
+        <img :src="data.image" alt="" srcset="">
+        <i>{{ data.title }} Price : {{ data.price }}</i>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { useStore } from 'vuex'
+// const store = useStore();
+// const getProducts = computed(() => {
+//     return store.getters.getProducts
+// })
+// const products = computed(() => {
+//     return store.state.products
+// })
+// onMounted(() => {
+//     store.dispatch('fetchProduct')
+// }),
+export default {
+    mounted() {
+        this.$store.dispatch('fetchProduct')
+    },
+    computed: {
+        // getProducts() {
+        //     return this.$store.getter.getProducts
+        // },
+        products() {
+            return this.$store.state.products
+        }
+    },
+    
+}
+</script>
+
+<style scoped>
+img{
+    height: auto;
+    width: 15%;
+}
+</style>
